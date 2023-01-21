4161d9a96ad09de4fdefabd440cd10cd18870844
kshneo0 <kshneo@gmail.com>
2023-01-22 07:28:01 +0900

    sec3 - 50. Our Greetings App using Pack

--- app.py
+++ app.py
@@ -10,17 +10,22 @@ root = tk.Tk()
 
 user_name = tk.StringVar()
 
-name_label = ttk.Label(root, text="Name: ")
-name_label.pack(side="left", padx=(0,10))
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
+input_frame.pack(fill="both")
+
+name_label = ttk.Label(input_frame, text="Name: ")
+name_label.pack(side="left", padx=(0, 10))
+name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
 name_entry.pack(side="left")
 name_entry.focus()
 
+buttons = ttk.Frame(root, padding=(20, 10))
+buttons.pack(fill="both")
 
-greet_button = ttk.Button(root, text="Greet", command=greet)
+greet_button = ttk.Button(buttons, text="Greet", command=greet)
 greet_button.pack(side="left")
 
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
 quit_button.pack(side="right")
 
 
