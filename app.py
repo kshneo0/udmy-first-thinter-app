90a60910e71a9a24068624f76bc98ed986de430f
kshneo0 <kshneo@gmail.com>
2023-01-22 06:32:31 +0900

    sec3 - 47. Our Greetings App

--- app.py
+++ app.py
@@ -3,15 +3,25 @@ from tkinter import ttk
 
 
 def greet():
-    print("Helli, World!")
+    print(f"Helli, {user_name.get() or 'World'}!")
 
 
 root = tk.Tk()
+
+user_name = tk.StringVar()
+
+name_label = ttk.Label(root, text="Name: ")
+name_label.pack(side="left", padx=(0,10))
+name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
+
 greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack(side="left", fill="y", expand=True)
+greet_button.pack(side="left")
 
 quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack(side="left")
+quit_button.pack(side="right")
 
 
 root.mainloop()
